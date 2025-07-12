6a3d11800f26579ce1d0eeae8bc3ca857db2c75a
Gobika910 <24bct121@kgcas.com>
2025-07-12 10:51:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let number=12;
+let name=`js`;
+let iscloudy=true;
+console.log(number+" "+name+" "+iscloudy);
+document.write(number);
+document.write(name);
+document.write(iscloudy);
+
+//task2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task3
+let num1=prompt("Enter the first val");
+let num2=prompt("Enter the second val");
+alert(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2)
